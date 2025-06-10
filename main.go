89d1691dfe35f989478baa40582dad655ef717ea
kepin17 <servicenya4@gmail.com>
2025-06-11 00:13:32 +0700

    add: variable , datas type,string formater , and print

--- main.go
+++ main.go
@@ -3,5 +3,30 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("JAwaaa")
+	var conferenceName = "Olyzano Conference"
+	const conferenceTickers = 50
+	var remainingTickets = 50
+
+	fmt.Printf("Welcome to our %s booking appliacation\n", conferenceName)
+	fmt.Printf("We have a total of %d tickets and are still available %d for booking\n",  conferenceTickers, remainingTickets )
+	fmt.Println("Get your tickets here to attend the conference")
+
+	var userName  string
+	var userTickets int
+	fmt.Println("Enter your name:")
+	fmt.Scan(&userName)
+	fmt.Println("Enter number of tickets:")
+	fmt.Scan(&userTickets)
+	remainingTickets = remainingTickets - userTickets
+	if remainingTickets <= 0 {
+		fmt.Println("Our conference is sold out. Please check back later for future events.")
+		} else {
+			fmt.Printf("We still have %d tickets available for booking.\n", remainingTickets)
+			fmt.Printf("Thank you %s for booking %d tickets. You will receive a confirmation email shortly.\n", userName, userTickets)
+	}
+	fmt.Println("We hope to see you at the conference!")
+	fmt.Println("Thank you for using our booking application!")
+	fmt.Println("Have a great day!")
+
+
 }
