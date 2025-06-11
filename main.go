1144daacea10a04ae0a1177aef540af9bd8489f6
kepin17 <servicenya4@gmail.com>
2025-06-11 13:27:36 +0700

    add slicing

--- main.go
+++ main.go
@@ -11,22 +11,30 @@ func main() {
 	fmt.Printf("We have a total of %d tickets and are still available %d for booking\n",  conferenceTickers, remainingTickets )
 	fmt.Println("Get your tickets here to attend the conference")
 
-	var userName  string
+
+	var bookings = [50]string{}
+
+	var userFirstName  string
+	var userLastName  string
+	var userEmail string
 	var userTickets int
-	fmt.Println("Enter your name:")
-	fmt.Scan(&userName)
+	fmt.Println("Enter your first name:")
+	fmt.Scan(&userFirstName)
+
+	fmt.Println("Enter your Last name:")
+	fmt.Scan(&userLastName)
 	fmt.Println("Enter number of tickets:")
 	fmt.Scan(&userTickets)
 	remainingTickets = remainingTickets - userTickets
-	if remainingTickets <= 0 {
-		fmt.Println("Our conference is sold out. Please check back later for future events.")
-		} else {
-			fmt.Printf("We still have %d tickets available for booking.\n", remainingTickets)
-			fmt.Printf("Thank you %s for booking %d tickets. You will receive a confirmation email shortly.\n", userName, userTickets)
+
+	bookings[0] = userName
+
+
+	if userTickets <= remainingTickets {	
+		fmt.Printf("The whole booking list is: %s\n", bookings)
+		fmt.Printf("The whole firstbooking list is: %v\n", bookings[0])
+		fmt.Printf("%s booked %d tickets. You have %d tickets remaining.\n", userName, userTickets, remainingTickets)
+	}else {
+		fmt.Printf("Sorry %s, we only have %d tickets remaining. You cannot book %d tickets.\n", userName, remainingTickets, userTickets)
 	}
-	fmt.Println("We hope to see you at the conference!")
-	fmt.Println("Thank you for using our booking application!")
-	fmt.Println("Have a great day!")
-
-
 }
